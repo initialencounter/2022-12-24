2d68fbdfae3cbbbcd578c3fd5711f249fb74963f
initialencounter <109729945+initialencounter@users.noreply.github.com>
2023-02-06 00:37:24 +0800

    Create main.py

---
+++ mswar_rank/server/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import sqlite3
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"data": "tapsss.com"}
+
+@app.get("/upload/{item}")
+async def read_item(item:str):
+    print(item)
+    with open('./database.txt','w+') as f:
+        f.write(item)
+    return 'success!'
+
+
+@app.get("/get")
+async def send_rank():                                     # 发送群排名
+    text = ''
+    with open('./database.txt','r') as f:
+        text += f.read()
+    return text
