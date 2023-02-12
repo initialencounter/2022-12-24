69e01c19f27d05ce1223d282931e2503f0ba5d7f
initialencounter <109729945+initialencounter@users.noreply.github.com>
2023-02-12 21:32:55 +0800

    Create 巅峰榜.js

---
+++ smear_rank/autojs/巅峰榜.js
@@ -0,0 +1,48 @@
+function main() {
+    app.launch("com.tencent.gamehelper.smoba")
+    sleep(1000)
+    className("android.widget.RadioButton").text("战绩").findOne().click()
+    sleep(5000)
+    Tap(750,1200)//这里不适配所有设备，请修改
+    let count = 1
+    const time = new Date()
+    const all_rank = { 'update_time': time.getFullYear() + '年' + time.getMonth() + '月' + time.getDate() + '日' + time.getHours() + '时' + time.getMinutes() + '分' }
+    const rank = []
+    const tar = accessibilityFocused(false).checked(false).indexInParent(1).className("android.widget.ListView").clickable(false).depth(20).packageName("com.tencent.gamehelper.smoba").row(-1).rowCount(3).rowSpan(-1).visibleToUser(true).findOne()
+    tar.children().forEach(child => {
+        const temp = []
+        temp.push(count)
+        count++
+        child.children().slice(1, 4).forEach(child1 => {
+            temp.push(child1.text())
+        })
+        rank.push(temp)
+
+
+    })
+    const tar1 = accessibilityFocused(false).className("android.widget.ListView").clickable(false).column(-1).columnSpan(-1).contextClickable(false).depth(20).indexInParent(2).longClickable(false).row(-1).rowCount(17).rowSpan(-1).findOne()
+    tar1.children().forEach(child => {
+        child.children().forEach(child1 => {
+            const temp = []
+            temp.push(count)
+            count++
+            child1.children().slice(2, 5).forEach(child2 => {
+                
+                temp.push(child2.text())
+            })
+            rank.push(temp)
+        })
+
+    })
+    all_rank['rank'] = rank
+    back()
+    home()
+    http.postJson('http://116.205.167.54:5140/upload-hok/',all_rank);
+    console.log('done')
+}
+log('巅峰榜等待中...')
+sleep(150000)
+while (1) {
+    main()
+    sleep(300000)
+}
