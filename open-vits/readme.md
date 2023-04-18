581af339d09aafc06de696cefe36c3faf0b5fe01
initialencounter <2911583893@qq.com>
2023-04-19 03:56:13 +0800

    修复空格bug，新增encodeURIComponent编码

--- open-vits/readme.md
+++ open-vits/readme.md
@@ -3,9 +3,18 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-open-vits?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-open-vits)
 
 
+## 注意事项
+>对于部署者行为及所产生的任何纠纷， Koishi 及 koishi-plugin-open-vits 概不负责。<br>
+如果有更多文本内容想要修改，可以在<a style="color:blue" href="/locales">本地化</a>中修改 zh 内容</br>
+后端搭建教程<a style="color:blue" href="https://github.com/Artrajz/vits-simple-api">vits-simple-api</a>
+## 使用方法
+- say 要转化的文本
+  - 可选项-s 说话的音色id
 
+## 问题反馈群: 
+399899914
 
 ## 更新日志
-
+* V0.0.4 修复空格bug，新增encodeURIComponent编码
 * v0.0.3 添加usage, 作者信息
 * v0.0.2 实现了vits服务
--- open-vits/src/index.ts
+++ open-vits/src/index.ts
@@ -1,45 +1,71 @@
-import { Context, Schema,h,Service,Session } from 'koishi'
-export const name = 'open-vits'
+import { Context, Schema, h, Service, Session, Logger } from 'koishi'
+export const name: string = 'open-vits'
+export const logger: Logger = new Logger(name)
 declare module 'koishi' {
   interface Context {
     vits: Vits
   }
   interface Vits {
-    say(prompt:string): Promise<string | h>
+    say(prompt: string, speaker_id?: number): Promise<string | h>
   }
 
 }
 class Vits extends Service {
   temp_msg: string
-  constructor(ctx:Context,private config:Vits.Config){
-    super(ctx,'vits',true)
+  speaker: number
+  constructor(ctx: Context, private config: Vits.Config) {
+    super(ctx, 'vits', true)
+    this.speaker = 3
     ctx.i18n.define('zh', require('./locales/zh'));
+
+    // 记录发送消息的messageid
     ctx.on('send', (session) => {
       this.temp_msg = session.messageId
     })
-    ctx.command('say').action(async ({session},prompt)=>{
-      await session.send(config.waiting_text)
-      if(config.recall){
-        this.recall(session,this.temp_msg)
-      }
-      return await this.say(prompt)
-    })
+    ctx.command('say <prompt:text>', 'vits文字转语音')
+      .option('speaker', '-s <speaker:number>', { fallback: config.speaker_id })
+      .action(async ({ session, options }, prompt) => {
+        // 判断speaker_id是否合法
+        this.speaker = options.speaker ? options.speaker : config.speaker_id
+        this.speaker = (this.speaker < this.config.max_speakers && this.speaker > 0) ? this.speaker : 3
+        await session.send(config.waiting_text)
+        // 判断是否需要撤回
+        if (config.recall) {
+          this.recall(session, this.temp_msg)
+        }
+        return await this.say(prompt, this.speaker)
+      })
   }
+  // 撤回的方法
   async recall(session: Session, messageId: string) {
     new Promise(resolve => setTimeout(() => {
       session.bot.deleteMessage(session.channelId, messageId)
     }
-      ,this.config.recall_time ));
+      , this.config.recall_time));
   }
-  async say(prompt:string): Promise<string | h>{
-    if(prompt.length>this.config.max_length){
-      return '文字过长'
+
+  /**
+   * 
+   * @param prompt 要转化的文本
+   * @param speaker_id 音色id，可选
+   * @returns 
+   */
+  async say(prompt: string, speaker_id: number = this.config.max_speakers): Promise<string | h> {
+    if (prompt.length > this.config.max_length) {
+      return '文本过长'
     }
-    const response:Buffer = await this.ctx.http.get(this.config.endpoint+prompt+'&format=ogg&id=3', { responseType: 'arraybuffer' });
-    return h.audio(response, 'audio/mpeg')
+    try {
+      const url: string = `${this.config.endpoint}/voice?text=${encodeURIComponent(prompt)}&id=${speaker_id}&format=ogg`
+      const response: Buffer = await this.ctx.http.get(url, { responseType: 'arraybuffer' });
+      return h.audio(response, 'audio/mpeg')
+    } catch (e) {
+      logger.info(String(e))
+      return String(e)
+    }
+
   };
 }
-namespace Vits{
+namespace Vits {
   export const usage = `
 ## 注意事项
 >对于部署者行为及所产生的任何纠纷， Koishi 及 koishi-plugin-open-vits 概不负责。<br>
@@ -49,7 +75,7 @@ namespace Vits{
 * say 要转化的文本
 
 ## 问题反馈群: 
-39989991
+399899914
 `
   export interface Config {
     endpoint: string
@@ -58,15 +84,21 @@ namespace Vits{
     waiting_text: string
     recall: boolean
     recall_time: number
+    speaker_id: number
+    max_speakers: number
   }
-  export const Config: Schema<Config> = Schema.object({
-    endpoint: Schema.string().default('https://api.vits.t4wefan.pub/voice?text=').description('服务器地址'),
-    max_length: Schema.number().default(256).description('最大长度'),
-    waiting: Schema.boolean().default(true).description('消息反馈，会发送思考中...'),
-    waiting_text: Schema.string().default('思考中...').description('等待时发送的文本'),
-    recall: Schema.boolean().default(true).description('会撤回思考中'),
-    recall_time: Schema.number().default(5000).description('撤回的时间')
-  })
+  export const Config =
+    Schema.object({
+      endpoint: Schema.string().required().description('vits服务器地址'),
+      speaker_id: Schema.number().default(3).description('speaker_id'),
+      max_length: Schema.number().default(256).description('最大长度'),
+      waiting: Schema.boolean().default(true).description('消息反馈，会发送思考中...'),
+      waiting_text: Schema.string().default('思考中...').description('等待时发送的文本'),
+      recall: Schema.boolean().default(true).description('会撤回思考中'),
+      recall_time: Schema.number().default(5000).description('撤回的时间'),
+      max_speakers: Schema.number().default(3).description('max_speakers')
+    })
+
 }
 
 export default Vits
