587b53b5f57565bc46f32050d1237d7d81effe69
initialencounter <2911583893@qq.com>
2023-07-08 02:09:19 +0800

    change: readme

--- clock/src/index.ts
+++ clock/src/index.ts
@@ -29,6 +29,23 @@ export const usage = `
 |     month    |     1-12 (or names) |
 |  day of week |     0-7 (or names, 0 or 7 are sunday)  |
 
+### 注意事项
+必须填写推送规则，否则不能发送消息
+#### 示例：
+platform: onebot       // 平台名称
+channelId: '399899914' // 群号
+guildId: '399899914' // 群号
+selfId: '1114039391' // 机器人的qq号
+
+### 示例
+\`\`\`
+在工作日 17:30 发送下班了
+clock 0 30 17 * * 1,2,3,4,5
+在每天 00:00 发送晚安
+clock 0 0 0 * * *
+在每天早上 8:00 发送早安
+cloak 0 0 8 * * * 
+\`\`\`
 
 `
 export interface Time {
@@ -98,7 +115,7 @@ export function apply(ctx: Context, config: Config) {
   ctx.command('clock.l', "列出所有闹钟").action(async ({ session }) => {
     return list_clock(ctx, session)
   })
-  ctx.command('clock.s [id:number]', "列出所有闹钟", { checkArgCount: true }).action(async ({ session }, id) => {
+  ctx.command('clock.s [id:number]', "关闭/启动闹钟", { checkArgCount: true }).action(async ({ session }, id) => {
     return clock_switch(ctx, session, id)
   })
 }
