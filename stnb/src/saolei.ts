e7a0ea8b4d866b8b41bd573b8d9970c239854d6b
initialencounter <2911583893@qq.com>
2023-07-09 00:10:01 +0800

    add: view profile

--- stnb/src/saolei.ts
+++ stnb/src/saolei.ts
@@ -21,4 +21,32 @@ export function apply(ctx: Context) {
         const msg = titles + '\n' + author + '\n' + content
         return msg
     })
+    ctx.command('雷网用户 <id:number>', '查看雷网用户',{checkArgCount:true}).action(async({},id)=>{
+        const res: Buffer = await ctx.http.get(`http://saolei.wang/Player/Info.asp?Id=${id}`, { responseType: "arraybuffer" })
+        const buf = iconv.decode(res, 'gb2312');
+        const html = buf.toString()
+        const doc = new dom().parseFromString(html, 'text/xml');
+        const pop = xpath.select("/html/body/table/tr/td/table/tr/td/span", doc)
+        const score = xpath.select("/html/body/table/tr/td[@class='Text']/a", doc)
+        const info = xpath.select("/html/body/table/tr/td[@class='Text']/span", doc)
+        const word = []
+        const scores = []
+        info.forEach((i)=>{
+            word.push(i?.["childNodes"]?.['0']?.data)
+        })
+        score.forEach((i)=>{
+            scores.push(i?.["childNodes"]?.['0']?.data)
+        })
+        let msg = `${word[0]} | ${word[1]}${word[2]}
+${word.slice(3,7).join('')}\n
+初级：${scores[0]} | ${scores[1]}
+中级：${scores[2]} | ${scores[3]}
+高级：${scores[4]} | ${scores[5]}
+总计: ${word[7]} | ${word[8]}\n
+综合人气：${pop[0]?.["childNodes"]?.['0']?.data}
+本日人气：${pop[1]?.["childNodes"]?.['0']?.data}
+        `
+        return msg
+
+    })
 }
