2db34fa2bca4cccf9dfbfe98b176007b627642c6
initialencounter <2911583893@qq.com>
2023-07-27 23:37:40 +0800

    add: ban_rank

--- specialtile/src/index.tsx
+++ specialtile/src/index.tsx
@@ -6,9 +6,24 @@ import fs from "fs"
 export const name = 'specialtitle'
 export const logger = new Logger(name)
 
+declare module 'koishi' {
+  interface Tables {
+    ban_rank: Ban_rank
+  }
+}
+
+export interface Ban_rank {
+  id: number
+  uid: string
+  gid: string
+  score: number
+}
+
 class Special {
   nickname_data: Dict
-  constructor(ctx: Context, config: Special.Config) {
+  map: Dict
+  constructor(private ctx: Context, config: Special.Config) {
+    this.map = {}
     try {
       this.nickname_data = require('./data/nickname.json');
     } catch (e) {
@@ -83,51 +98,126 @@ class Special {
         "相见不如怀念"]
       fs.writeFileSync('./data/nickname.json', JSON.stringify(this.nickname_data));
     }
+    ctx.model.extend('ban_rank', {
+      id: "unsigned",
+      uid: 'string',
+      gid: 'string',
+      score: 'integer'
+    },{
+      autoInc: true
+    })
+
     ctx.command('修改昵称 [uid:string] [nickname:string]', '修改群友昵称', { checkArgCount: true, authority: 5 }).action(async ({ session }, ...args) => {
+      if (session.platform !== 'onebot') {
+        return '该命令只适用于 onebot 平台'
+      }
       session?.onebot.setGroupCard(session.guildId, args[0], args[1])
     })
     ctx.command('修改头衔 [uid:string] [nickname:string]', '修改群友头衔', { checkArgCount: true, authority: 5 }).action(async ({ session }, ...args) => {
+      if (session.platform !== 'onebot') {
+        return '该命令只适用于 onebot 平台'
+      }
       session?.onebot.setGroupSpecialTitle(session.guildId, args[0], args[1])
     })
-    ctx.command('口球大礼包').action(({ session }) => {
-      session?.onebot.setGroupBan(session.guildId, session.userId, Math.floor((Math.random() * 300)))
+    ctx.command('口球大礼包').action(async ({ session }) => {
+      if (session.platform !== 'onebot') {
+        return '该命令只适用于 onebot 平台'
+      }
+      const dt = Math.floor((Math.random() * 300))
+      await this.add_score(session.channelId,session.userId,dt)
+      session?.onebot.setGroupBan(session.channelId, session.userId, dt)
     })
-    ctx.middleware((session, next) => {
+    ctx.command('封神榜','谁才是本群的运气王').action(async ({ session }) => {
+      if (session.platform !== 'onebot') {
+        return '该命令只适用于 onebot 平台'
+      }
+      const list = await ctx.database.get('ban_rank', { gid: session.channelId })
+      if (list.length < 1) {
+        return '本群无人封神'
+      }
+      const sorted_arr = this.quickSort(list)
+      const rank_div: any[] = []
+      rank_div.push(<div style="font-size:20px;width:200px;height:30px">封神榜</div>)
+      for (var i in sorted_arr) {
+        var itm: Ban_rank = sorted_arr[i]
+        const info = await session?.onebot.getGroupMemberInfo(session.guildId,itm.uid)
+        rank_div.push(<div style="font-size:10px;width:200px;height:20px">{`${(info.nickname||info.user_id)}:${itm.score}`}</div>)
+      }
+      return <html>
+        <div style={{
+          width: 200 + 'px',
+          height: (sorted_arr.length + 1) * 50 + 30 + 'px',
+          background: "transparent",
+        }}></div>
+        {rank_div}
+      </html>
+    })
+    ctx.middleware(async(session, next) => {
       if (!session.content.startsWith('捆绑')) {
         return next()
+      }
+      if (session.platform !== 'onebot') {
+        return '该命令只适用于 onebot 平台'
       }
       const target = session.content.match(/(?<=<at id=")([\s\S]*?)(?="\/>)/g)
       if (!target) {
         return next()
       }
       for (var i of target) {
-        console.log(i)
-        session?.onebot.setGroupBan(session.guildId, i, Math.floor((Math.random() * 60)))
+        const dt = Math.floor((Math.random() * 60))
+        await this.add_score(session.channelId,i,dt)
+        session?.onebot.setGroupBan(session.channelId, i, dt)
       }
       return next()
 
     })
-    ctx.middleware((session, next) => {
+    ctx.middleware(async(session, next) => {
       if (!session.content.startsWith('解绑')) {
         return next()
+      }
+      if (session.platform !== 'onebot') {
+        return '该命令只适用于 onebot 平台'
       }
       const target = session.content.match(/(?<=<at id=")([\s\S]*?)(?="\/>)/g)
       if (!target) {
         return next()
       }
       for (var i of target) {
-        console.log(i)
-        session?.onebot.setGroupBan(session.guildId, i, 0)
+        await this.add_score(session.channelId,i,0-(this.map[i+session.channelId]?this.map[i+session.channelId]:0))
+        session?.onebot.setGroupBan(session.channelId, i, 0)
       }
       return next()
     })
     ctx.on('guild-member-added', async (session) => {
       if (session.platform == 'onebot' && session?.onebot) {
         const nickname = this.get_rondom_name()
-        // 记录名片
         await session?.onebot.setGroupSpecialTitle(session.guildId, session.userId, nickname)
       }
     })
+  }
+  quickSort(arr: Ban_rank[]) {
+    //基础结束条件：数组长度为1时，不用再作比较，直接返回
+    if (arr.length < 2) return arr
+    let pivot = arr[0].score //基准值
+    let left = []
+    let right = []
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i].score > pivot) {
+        left.push(arr[i])
+      } else {
+        right.push(arr[i])
+      }
+    }
+    return this.quickSort(left).concat([arr[0]]).concat(this.quickSort(right))
+  }
+  async add_score(channelId:string,userId:string, score: number) {
+    this.map[userId+channelId] = score
+    const target = await this.ctx.database.get('ban_rank', { uid: userId, gid: channelId })
+    if (target.length === 0) {
+      await this.ctx.database.create('ban_rank', { uid: userId, gid: channelId, score: score })
+    } else {
+      await this.ctx.database.set('ban_rank', { uid: userId, gid: channelId }, { score: target[0].score + score })
+    }
   }
   get_rondom_name(): string {
     return this.nickname_data[Math.floor((Math.random() * this.nickname_data.length))]
@@ -139,6 +229,7 @@ namespace Special {
 机器人必须拥有群主权限
 如果有更多文本内容想要修改，可以在 data/nickname.json 中修改
 `
+
   export interface Config {
   }
 
