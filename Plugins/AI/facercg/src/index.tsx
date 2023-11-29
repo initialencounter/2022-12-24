e79e5e67cb852ec22ff7c1a5ff13a06ed9b19729
initialencounter <2911583893@qq.com>
2023-11-29 23:05:32 +0800

    feat: thicker rect

--- Plugins/AI/facercg/src/index.tsx
+++ Plugins/AI/facercg/src/index.tsx
@@ -18,7 +18,7 @@ export const div_items = (face_arr: Face_info[]) => {
   const style_arr: string[] = []
   for (var i in face_arr) {
     var location: Location = face_arr[i].location
-    var style_str: string = `transform: rotate(${location.rotation}deg);position: absolute;font-size: 10px;width: ${location.width}px;height: ${location.height}px;left: ${location.left}px;top: ${location.top}px;rotation: ${location.rotation}deg;background: transparent;border: 1px solid green`
+    var style_str: string = `transform: rotate(${location.rotation}deg);position: absolute;font-size: 10px;width: ${location.width}px;height: ${location.height}px;left: ${location.left}px;top: ${location.top}px;rotation: ${location.rotation}deg;background: transparent;border: 5px solid green`
     style_arr.push(style_str)
   }
   const res: Element[] = style_arr.map((style, id) =>
@@ -174,15 +174,14 @@ export async function apply(ctx: Context, config: Config) {
       const div_items_: Element[] = div_items(face_arr)//框出face
       const text_msg: Element[] = msg(face_arr)//文字消息
 
+      session.send(text_msg)
       //判断是否启用puppeteer
       if (ctx.puppeteer) {
         return <html>
           <img src={img_url} />
           {div_items_}
-          <p>{text_msg}</p>
         </html>
-      } else {
-        return text_msg
       }
+      return
     })
 }
