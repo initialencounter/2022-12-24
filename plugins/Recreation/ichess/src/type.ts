b3f7132ba28d09b8bae0d1c455c0883cbf833e23
initialencounter <2911583893@qq.com>
2023-12-21 23:57:20 +0800

    chore: add ChessMoveString type

--- plugins/Recreation/ichess/src/type.ts
+++ plugins/Recreation/ichess/src/type.ts
@@ -24,14 +24,22 @@ export enum ChessPiece {
 }
 
 export const ChessMap = {
-  a:1,
-  b:2,
-  c:3,
-  d:4,
-  e:5,
-  f:6,
-  g:7,
-  h:8
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8
 }
 
 export const chessHeader = new Set(['k', 'q', 'r', 'b', 'n', 'p']);
+
+type Alphas = 'a' | 'b' | 'c' | 'e' | 'f' | 'g' | 'h'
+type Numbers = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8'
+type A = `${Numbers}${Alphas}`
+type B = `${Alphas}${Numbers}`
+type TwoCharString = A | B
+  
+export type ChessMoveString = `${'p' | 'n' | 'b' | 'r' | 'q' | 'k'}${TwoCharString}`;
