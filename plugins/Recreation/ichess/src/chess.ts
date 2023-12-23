b4ea8e681d64cdcd2e82bdf05748f2a486962f50
initialencounter <2911583893@qq.com>
2023-12-23 21:40:40 +0800

    chore: impl serial

--- plugins/Recreation/ichess/src/chess.ts
+++ plugins/Recreation/ichess/src/chess.ts
@@ -1,20 +1,26 @@
 import type * as puppeteer from 'koishi-plugin-puppeteer'
 import { Context, Session } from 'koishi'
 import { Chess } from 'chess.js'
-import { ChessPiece, Ichess, MoveResult } from './type'
+import { ChessMoveString, ChessPiece, Ichess, MoveResult } from './type'
 
 
 
 export class ChessState {
+    serial():Ichess {
+        return { p1: this.p1, p2: this.p2, next: this.next, history: this.history, imageMode: this.imageMode }
+    }
     p1: string
     p2: string
     next: string
     history: string = ""
     imageMode: boolean
     board: Chess
-    update: (this: ChessState, x: number, y: number, value: 1 | -1, chessPieces?: ChessPiece) => MoveResult | string
+    update(chessPieces: ChessMoveString):{res:MoveResult,boardString:string}{
+        this.board.move(chessPieces)
+        return {res:MoveResult.draw,boardString:''}
+    }
 
-    constructor(ichess?:Ichess) {
+    constructor(ichess?: Ichess) {
         if (ichess && typeof ichess === 'object') {
             Object.assign(this, { ...ichess });
 
