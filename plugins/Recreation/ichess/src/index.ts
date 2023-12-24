7aca595769615bf4b8f92ff9550249d8794644af
initialencounter <2911583893@qq.com>
2023-12-24 19:52:02 +0800

    ichess:feat: impl commands

--- plugins/Recreation/ichess/src/index.ts
+++ plugins/Recreation/ichess/src/index.ts
@@ -1,10 +1,12 @@
 import { Context, segment, Dict, Schema } from 'koishi'
 import { } from 'koishi-plugin-puppeteer'
 import { } from '@koishijs/plugin-help'
-import {  MoveResult, ChessPiece, chessHeader, ChessMap, Ichess, ChessMoveString } from './type'
+import { MoveResult, Ichess } from './type'
 import { ChessState } from './chess'
 import { resolve } from 'path'
 import { readFileSync } from 'fs'
+import { drawBoard } from './render'
+import { parseInput } from './utils'
 
 
 declare module 'koishi' {
@@ -18,16 +20,21 @@ const states: Dict<ChessState> = {}
 
 export const name = 'ichess'
 export const inject = {
-  optional: ['puppeteer', 'database'],
+  required: ['puppeteer'],
+  optional: ['database']
 }
 
-export interface Config { }
+export interface Config {
+  prefix:string
+ }
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  prefix:Schema.string().default('i').description('棋谱前缀，【创建/加入对局】 不会受影响')
+})
 
-export const usage = readFileSync(resolve(__dirname, "../readme.md")).toString('utf-8').split("更新日志")[0];
+export const usage = readFileSync(resolve(__dirname, '../readme.md')).toString('utf-8').split('更新日志')[0]
 
-export function apply(ctx: Context) {
+export function apply(ctx: Context,config:Config) {
   ctx.model.extend('channel', {
     // do not use shorthand because the initial value is `null`
     ichess: { type: 'json' },
@@ -35,20 +42,28 @@ export function apply(ctx: Context) {
 
   ctx = ctx.guild()
 
-  ctx.command('chess [position]', '棋类游戏')
+  ctx.middleware(async (session, next) => {
+    let position: string = session.content.replace(config.prefix,'')
+    if (session.content.startsWith(config.prefix)) {
+      if (position?.length>1) {
+        session.execute('ichess '+position)
+        return
+      }
+    }
+    return next()
+  })
+  ctx.command('ichess [position]', '棋类游戏')
     .userFields(['name'])
     .channelFields(['ichess'])
-    .alias('落子')
     .alias('悔棋', { options: { repent: true } })
-    .alias('国际象棋', { options: { size: 8, rule: 'ichess' } })
+    .alias('国际象棋')
     .alias('停止下棋', { options: { stop: true } })
-    .alias('跳过回合', { options: { skip: true } })
     .alias('查看棋盘', { options: { show: true } })
+    .alias('查看pgn', { options: { pgn: true } })
     .option('repent', '悔棋')
     .option('show', '-v, --show, --view  显示棋盘')
     .option('stop', '-e, --stop, --end  停止游戏')
-    .option('imageMode', '-i  使用图片模式', { hidden: () => !ctx.puppeteer })
-    .option('textMode', '-t  使用文本模式', { hidden: () => !ctx.puppeteer })
+    .option('pgn','-p 显示 pgn')
     .usage([
       '输入“国际象棋”开始对应的一局游戏。',
     ].join('\n'))
@@ -61,27 +76,24 @@ export function apply(ctx: Context) {
 
         const state = new ChessState()
         state.p1 = userId
+        state.player1 = { id: userId, name: session.username, avatar: session?.event?.member?.avatar ?? '' }
         states[cid] = state
-
-        return state.draw(session, `${session.username} 发起了游戏！`)
+        session.send(`${session.username} 发起了国际象棋, 输入指令 chess 加入对局！`)
+        const boardImg = await drawBoard(ctx, state, { res: 1 })
+        return boardImg
       }
 
       if (options.stop) {
-        channel.ichess =null
+        channel.ichess = null
         delete states[cid]
         return '游戏已停止。'
       }
 
       const state = states[cid]
 
-      if (options.show) return '这是一个棋盘'
-
-      if (options.textMode) {
-        state.imageMode = false
-        return '已切换到文本模式。'
-      } else if (options.imageMode) {
-        state.imageMode = true
-        return '已切换到图片模式。'
+      if (options.show) {
+        const boardImg = await drawBoard(ctx, state, { res: 1 })
+        return boardImg
       }
 
       if (state.p2 && state.p1 !== userId && state.p2 !== userId) {
@@ -92,39 +104,43 @@ export function apply(ctx: Context) {
         if (!state.next) return '对局尚未开始。'
         const last = state.p1 === state.next ? state.p2 : state.p1
         if (last !== userId) return '上一手棋不是你所下。'
-        state.next = last
-        channel.ichess = state.serial()
-        return `${session.username} 进行了悔棋。`
+        const res = await state.undo()
+        if (res.res == MoveResult.illegal) {
+          return `悔棋失败`
+        } else {
+          state.next = last
+          channel.ichess = state.serial()
+          return `${session.username} 进行了悔棋。`
+        }
+      }
+      if(options.pgn){
+        return state.board.pgn()
+      }
+      if (!position && !state.p2) {
+        if (userId == state.p1) {
+          session.send(`${session.username} 发起了国际象棋, 输入指令 chess 加入对局！`)
+          const boardImg = await drawBoard(ctx, state, { res: 1 })
+          return boardImg
+        }
+        state.p2 = userId
+        state.player2 = { id: userId, name: session.username, avatar: session?.event?.member?.avatar ?? '' }
+        state.next = state.p1
+        return `${session.username}加入了游戏，下一手轮到${segment.at(state.next) ?? state.next}。`
+      }
+      if (!position) {
+        return `请输入棋谱`
       }
 
-      if (!position) return '请输入坐标。'
+      if (state.p2 && userId !== state.next) return `当前是${state.next}的回合。`
 
-      if (state.p2 && userId !== state.next) return '当前不是你的回合。'
-
-      if(!/^[pnbrqk][1-8a-h][a-h1-8]$/i.test(position)){
-        return "输入不合法，示例: pe4, pf3"
+      if (!state.p2 && userId !== state.p1) {
+        state.p2 = userId
       }
 
-      const [x, y] = [
-        ChessMap[position.slice(1, 2)] ?? parseInt(position.slice(1, 2)),
-        ChessMap[position.slice(2, position.length)] ?? parseInt(position.slice(2, position.length))
-      ]
+      position = parseInput(position)
 
       let message = ''
-      if (state.next || userId === state.p1) {
-        message = `${session.username} 落子于 ${position.toUpperCase()}，`
-      } else {
-        if (state.history.length === 1) {
-          state.p2 = state.p1
-          state.p1 = userId
-        } else {
-          state.p2 = userId
-        }
-        message = `${session.username} 加入了游戏并落子于 ${position.toUpperCase()}，`
-      }
-
-      const value = userId === state.p1 ? 1 : -1
-      let result = state.update(position as ChessMoveString)
+      let result = state.update(position)
 
 
       switch (result.res) {
@@ -132,35 +148,20 @@ export function apply(ctx: Context) {
           state.next = userId
           return '非法落子。'
         case MoveResult.skip:
+          state.next = userId
           message += `下一手依然轮到 ${segment.at(userId)}。`
           break
-        case MoveResult.p1Win:
-          channel.ichess =null
+        case MoveResult.end:
+          channel.ichess = null
           delete states[cid]
-          return message + `恭喜 ${segment.at(state.p1)} 获胜！`
-        case MoveResult.p2Win:
-          channel.ichess =null
-          delete states[cid]
-          return message + `恭喜 ${segment.at(state.p2)} 获胜！`
-        case MoveResult.draw:
-          channel.ichess =null
-          delete states[cid]
-          return message + '本局游戏平局。'
-        case undefined:
-          // eslint-disable-next-line no-cond-assign
-          if (state.next = userId === state.p1 ? state.p2 : state.p1) {
-            message += `下一手轮到 ${segment.at(state.next)}。`
-          } else {
-            message = message.slice(0, -1) + '。'
-          }
-          break
-        default:
-          state.next = userId
-          return `非法落子（${result}）。`
+          return message + `恭喜 ${segment.at(state.next)} 获胜！`
+        case MoveResult.next:
+          state.next = (userId === state.p1) ? state.p2 : state.p1
+          message += `下一手轮到 ${segment.at(state.next) ?? state.next}。`
       }
+      channel.ichess = state.serial()
+      return await drawBoard(ctx, state, result)
 
-      channel.ichess =state.serial()
-      return state.draw(session, message, x, y)
     })
 
   ctx.inject(['database'], async (ctx) => {
