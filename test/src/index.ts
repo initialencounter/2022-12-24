6c5c278a4308b0865faf13be94a6d68ee9f442a1
initialencounter <2911583893@qq.com>
2024-02-24 00:19:50 +0800

    yarn dep

--- test/src/index.ts
+++ test/src/index.ts
@@ -17,11 +17,4 @@ export const Config: Schema<Config> = Schema.object({
 })
 
 export function apply(ctx: Context,config:Config) {
-  ctx.on('ready',async ()=>{
-    console.log(ctx.jimp.FONT_SANS_10_BLACK)
-    const a = await ctx.jimp.read("C:\\Users\\29115\\dev\\ks\\Plugins\\News\\gh-tile\\src\\0.jpg")
-    a.crop(20,20,20,20)
-    a.writeAsync('C:\\Users\\29115\\dev\\ks\\test\\src\\0.jpg')
-    console.log('done4')
-  })
 }
