2083d23aeaf95cee242393860cb10bedd8d44f4e
initialencounter <2911583893@qq.com>
2024-04-14 22:02:20 +0800

    chore(gh-tile): use SCF

--- plugins/News/gh-tile/src/index.ts
+++ plugins/News/gh-tile/src/index.ts
@@ -20,7 +20,7 @@ export interface Rule {
 
 export const Config: Schema<Config> = Schema.object({
   corn: Schema.string().default("23-30").description("默认的提醒时间,UTC时间"),
-  forwardServer: Schema.string().default("https://1314084067-1wp448wrzt-gz.scf.tencentcs.com/?method=POST").description("转发服务 SCF"),
+  forwardServer: Schema.string().default("https://1314084067-1k6wykqw4f-gz.scf.tencentcs.com/?method=POST").description("转发服务 SCF"),
   cookie: Schema.string().default("logged_in=yes;tz=Asia%2FShanghai;").description("cookie")
 })
 
