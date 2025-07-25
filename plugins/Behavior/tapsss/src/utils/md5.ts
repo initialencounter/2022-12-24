9904c304f82f6787cd4cdfdfbe6b887f7f0fffe5
initialencounter <2911583893@qq.com>
2025-07-25 14:44:13 +0800

    chore

--- plugins/Behavior/tapsss/src/utils/md5.ts
+++ plugins/Behavior/tapsss/src/utils/md5.ts
@@ -8,8 +8,7 @@ export function computeMD5(str: string, salt: string = ""): string {
   try {
     const hash = createHash('md5');
     hash.update(str + salt);
-    const digest = hash.digest('hex');
-    return digest;
+    return hash.digest('hex');
   } catch (error) {
     console.error("MD5 计算失败:", error);
     return "";
