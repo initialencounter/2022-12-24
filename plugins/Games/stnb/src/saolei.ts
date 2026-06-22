a1f89007ff07c957ee5a89fc6eb84c65673691d3
initialencounter <2911583893@qq.com>
2026-06-22 17:18:51 +0800

    feat(saolei): 使用 cheerio 替换 xpath 进行 HTML 解析

--- plugins/Games/stnb/src/saolei.ts
+++ plugins/Games/stnb/src/saolei.ts
@@ -1,7 +1,6 @@
 import { Context, Schema } from 'koishi'
 import iconv from 'iconv-lite';
-import xpath from 'xpath';
-const dom = require('xmldom').DOMParser;
+import * as cheerio from 'cheerio';
 
 export const name = 'saolei'
 
@@ -14,37 +13,37 @@ export function apply(ctx: Context) {
         const res: ArrayBuffer = await ctx.http.get(`http://saolei.wang/BBS/Title.asp?Id=${id}`, { responseType: "arraybuffer" })
         const buf = iconv.decode(Buffer.from(res), 'gb2312');
         const html = buf.toString()
-        const doc = new dom().parseFromString(html, 'text/xml');
-        const titles = xpath.select("//table/tr/td/span[@class='High']", doc)[0]["childNodes"]["0"].data
-        const content = xpath.select("//tr[@bgcolor='#333333']/td[@valign='top']/table/tr/td", doc)[0]["childNodes"]["0"].data
-        const author = xpath.select("//table/tr/td/a[@title='点击查看个人信息']", doc)[0]["childNodes"]["0"].data
-        const msg = titles + '\n' + author + '\n' + content
+        const $ = cheerio.load(html);
+        const title = $('span.High').first().text()
+        const content = $('tr[bgcolor="#333333"] td[valign="top"] table tr td').first().text()
+        const author = $('a[title="点击查看个人信息"]').first().text()
+        const msg = title + '\n' + author + '\n' + content
         return msg
     })
     ctx.command('stnb.雷网用户 <id:number>', '查看雷网用户',{checkArgCount:true}).action(async({},id)=>{
         const res: ArrayBuffer = await ctx.http.get(`http://saolei.wang/Player/Info.asp?Id=${id}`, { responseType: "arraybuffer" })
         const buf = iconv.decode(Buffer.from(res), 'gb2312');
         const html = buf.toString()
-        const doc = new dom().parseFromString(html, 'text/xml');
-        const pop = xpath.select("/html/body/table/tr/td/table/tr/td/span", doc)
-        const score = xpath.select("/html/body/table/tr/td[@class='Text']/a", doc)
-        const info = xpath.select("/html/body/table/tr/td[@class='Text']/span", doc)
-        const word = []
-        const scores = []
-        info.forEach((i)=>{
-            word.push(i?.["childNodes"]?.['0']?.data)
+        const $ = cheerio.load(html);
+        const pop = $('body > table > tr > td > table > tr > td > span')
+        const scores = $('td.Text > a')
+        const info = $('td.Text > span')
+        const word: string[] = []
+        const scoreList: string[] = []
+        info.each((_, el) => {
+            word.push($(el).text())
         })
-        score.forEach((i)=>{
-            scores.push(i?.["childNodes"]?.['0']?.data)
+        scores.each((_, el) => {
+            scoreList.push($(el).text())
         })
         let msg = `${word[0]} | ${word[1]}${word[2]}
 ${word.slice(3,7).join('')}\n
-初级：${scores[0]} | ${scores[1]}
-中级：${scores[2]} | ${scores[3]}
-高级：${scores[4]} | ${scores[5]}
+初级：${scoreList[0]} | ${scoreList[1]}
+中级：${scoreList[2]} | ${scoreList[3]}
+高级：${scoreList[4]} | ${scoreList[5]}
 总计: ${word[7]} | ${word[8]}\n
-综合人气：${pop[0]?.["childNodes"]?.['0']?.data}
-本日人气：${pop[1]?.["childNodes"]?.['0']?.data}
+综合人气：${pop.eq(0).text()}
+本日人气：${pop.eq(1).text()}
         `
         return msg
 
